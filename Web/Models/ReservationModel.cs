1e80c4728d3315de926a5b50e3f675ac48d9f793
Jesper Jansson <jesperjansson@live.se>
2016-12-19 11:15:57 +0100

    Lagt till google-maps integration och en modell för Reservationer

---
+++ Web/Models/ReservationModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models
+{
+    public class ReservationModel
+    {
+        public int REID { get; set; }
+
+        public string Total_sum { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Check_In { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Check_Out { get; set; }
+
+        public int UID { get; set; }
+
+        public int RID { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
